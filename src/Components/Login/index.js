209491ee8cfb12552c6089a8923ab4267bde3591
OtavioCanedo <ocneves@sga.pucminas.br>
2023-01-31 15:06:18 -0300

    feat: tela de login

---
+++ src/Components/Login/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './styles.css'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username === 'admin' && password === 'password') {
+      setError(null);
+      // redirect to dashboard or show success message
+    } else {
+      setError('Invalid username or password');
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <p>{error}</p>}
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login
