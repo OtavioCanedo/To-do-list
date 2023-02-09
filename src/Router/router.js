e406a7f29f0f8256e80354b3459f870dad080130
lucaseduardocruz <lucas.edu.ca@hotmail.com>
2023-02-09 15:36:32 -0300

    feat: Criação da rota de tarefas

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Todolist from '../Components/to-do-list';
 
 import Login from '../Components/Login';
 
 const Router = () => (
   <Routes>
     <Route path="/" exact element={<Login />} />
+    <Route path="/task" element={<Todolist />} />
   </Routes>
 )
 
